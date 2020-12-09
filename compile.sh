5c6f2d4d95c555036360d6631499f237a72fd1e1
Marcel Ruegenberg <gh@picodesign.de>
2020-12-09 15:11:45 +0000

    compilation improvements

--- compile.sh
+++ compile.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
-if [ -z "$HFS" ]; then
-    echo "hello"
-    export HFS=/opt/hfs18.0.416
+if [ -z "$HOUDINI_DIR" ]; then
+    export HOUDINI_DIR=/opt/hfs18.0.416
+fi
+
+# if [ -z "$HFS" ]; then
+    export HFS=$HOUDINI_DIR
     export PATH=$HFS\bin:$PATH
     export CUR=`pwd`
     cd $HFS
+    echo "HOUDINI DIR: "
     echo `pwd`
     source houdini_setup
     cd $CUR
+# fi
+
+if [ -z "$ARNOLD_DIR" ]; then
+        export ARNOLD_DIR=deps/Arnold-6.0.3.0-linux
 fi
 
-# note: for Houdini 15.5, you need GCC 4.8!
-
-ARNOLD_DIR=deps/Arnold-6.0.3.0-linux
-HOUDINI_DIR=/opt/hfs18.0.416
-
-hcustom -e -i ./build -g src/ai_ocean_samplelayers.cpp -L ${ARNOLD_DIR}/bin -lai -I ${ARNOLD_DIR}/include -L ${HOUDINI_DIR}/dsolib -lHoudiniUI -lHoudiniOPZ -lHoudiniOP3 -lHoudiniOP2 -lHoudiniOP1 -lHoudiniSIM -lHoudiniGEO -lHoudiniPRM -lHoudiniUT -lboost_system
+# extra flags: -g: debug
+DEBUG_FLAG=
+# DEBUG_FLAG=-g
+hcustom -e ${DEBUG_FLAG} -i ./build src/ai_ocean_samplelayers.cpp -L ${ARNOLD_DIR}/bin -lai -I ${ARNOLD_DIR}/include -L ${HOUDINI_DIR}/dsolib -lHoudiniUI -lHoudiniOPZ -lHoudiniOP3 -lHoudiniOP2 -lHoudiniOP1 -lHoudiniSIM -lHoudiniGEO -lHoudiniPRM -lHoudiniUT -lboost_system -lHoudiniOP4 -lpxr_pxOsd -losdCPU
